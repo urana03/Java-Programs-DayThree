c63ce071647926a9d9d9407d88698d1a530474e7
urana03 <uhackninja@gmail.com>
2022-11-16 14:49:13 +0530

    Add:Program to print elements of array

---
+++ ElementsArray.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.daythree;
+
+public class ElementsArray {
+
+	public static void main(String args[]) {
+		int a[] = {1,2,3,4,5};
+		
+		for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+}
